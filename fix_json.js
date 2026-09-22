41a843fce00b1e5d29e4a973dbaec4a57ec1307c
Consultores NyT <admin@consultoresnyt.com>
2026-09-22 14:37:22 -0600

    Corregir opcion vacia en pregunta de Tecnologia en red de proveedores

---
+++ fix_json.js
@@ -0,0 +1,17 @@
+﻿
+const fs = require("fs");
+let data = JSON.parse(fs.readFileSync("diagnostico/questions.json", "utf8"));
+
+data.areas.forEach(area => {
+    area.questions.forEach(q => {
+        q.options.forEach(opt => {
+            if (opt.text === "|") {
+                opt.text = "Gestión por correo electrónico y órdenes de compra en ERP básico.";
+            }
+        });
+    });
+});
+
+fs.writeFileSync("diagnostico/questions.json", JSON.stringify(data, null, 2), "utf8");
+console.log("Fixed missing text in questions.json");
+
